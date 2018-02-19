eaf930749a1d390f6dccd2d35053d4e60138ae11
fattest <fattest123@yandex.ru>
2018-02-19 15:15:06 +0300

    123

---
+++ js/main.js
@@ -0,0 +1,66 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Привет, Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'Вы загрузили эту старницу в: ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Посадить дерево' },
+            { text: 'Посторить дом' },
+            { text: 'Вырастить сына' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Привет, Vue.js!'
+    }
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            {id: 0, text: 'Овощи'},
+            {id: 1, text: 'Сыр'},
+            {id: 2, text: 'Что там еще лди едеят?'}
+        ]
+    }
+})
